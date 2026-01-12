6e2b1ea70975d5c4b0d44728b6e208bad0b0d991
Nexussocial1 <hhjji4264@gmail.com>
2026-01-13 00:28:50 +0100

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,45 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, collection, addDoc, query, orderBy, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-const firebaseConfig={apiKey:"AIzaSyD8pCEwiDHWvb4vju9vtsXvxFvB29SM6VM",authDomain:"nexus-social-dce76.firebaseapp.com",projectId:"nexus-social-dce76",storageBucket:"nexus-social-dce76.appspot.com",messagingSenderId:"172694934932",appId:"1:172694934932:web:62bb46749dd0c062bfab81"};
-const app=initializeApp(firebaseConfig);
-const auth=getAuth(app);
-const db=getFirestore(app);
-window.login=()=>signInWithEmailAndPassword(auth,email.value,password.value);
-window.signup=()=>createUserWithEmailAndPassword(auth,email.value,password.value);
-window.logout=()=>signOut(auth);
-window.addPost=async()=>{if(!postText.value)return;await addDoc(collection(db,"posts"),{text:postText.value,user:auth.currentUser.email,time:serverTimestamp()});postText.value="";};
-onSnapshot(query(collection(db,"posts"),orderBy("time","desc")),snap=>{feed.innerHTML="";snap.forEach(d=>{const p=d.data();feed.innerHTML+=`<div><b>${p.user}</b><br>${p.text}</div>`;});});
-onAuthStateChanged(auth,u=>{authBox.style.display=u?"none":"block";if(u)showPage("home");});
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, addDoc, onSnapshot, serverTimestamp, query, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+ apiKey: "AIzaSyD8pCEwiDHWvb4vju9vtsXvxFvB29SM6VM",
+ authDomain: "nexus-social-dce76.firebaseapp.com",
+ projectId: "nexus-social-dce76",
+ storageBucket: "nexus-social-dce76.firebasestorage.app",
+ messagingSenderId: "172694934932",
+ appId: "1:172694934932:web:62bb46749dd0c062bfab81"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+signup.onclick = async()=>{
+ const cred = await createUserWithEmailAndPassword(auth,email.value,password.value);
+ await updateProfile(cred.user,{displayName:username.value,photoURL:`https://ui-avatars.com/api/?name=${username.value}`});
+ alert("Signed up!");
+};
+
+login.onclick = ()=>signInWithEmailAndPassword(auth,email.value,password.value);
+logout.onclick = ()=>signOut(auth);
+
+postBtn.onclick = ()=>addDoc(collection(db,"posts"),{
+ text:postInput.value,
+ user:auth.currentUser.displayName,
+ avatar:auth.currentUser.photoURL,
+ createdAt:serverTimestamp()
+});
+
+onSnapshot(query(collection(db,"posts"),orderBy("createdAt","desc")),snap=>{
+ feed.innerHTML="";
+ snap.forEach(d=>{
+  const p=d.data();
+  feed.innerHTML+=`
+  <div class="post-card">
+   <img src="${p.avatar}">
+   <div><b>${p.user}</b><p>${p.text}</p></div>
+  </div>`;
+ });
+});
