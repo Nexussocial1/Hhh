8127beac5babbc5f4dbd6150e964d7905cc520a5
Nexussocial1 <hhjji4264@gmail.com>
2026-01-13 02:07:15 +0100

    Delete src directory

--- src/App.tsx
+++
@@ -1,3 +0,0 @@
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "./firebase";import AuthPage from "./pages/AuthPage";import HomePage from "./pages/HomePage";
-export default function App(){const [user]=useAuthState(auth);return user?<HomePage/>:<AuthPage/>;}
